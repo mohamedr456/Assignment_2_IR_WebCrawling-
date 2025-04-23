42b3e0511fbcdef753af5f9a13f820c88bd3388c
Mohamed Rabea <64942093+mohamedr456@users.noreply.github.com>
2025-04-23 23:54:51 +0200

    Update Main.java

--- IRAssignment2/src/main/java/org/example/Main.java
+++ IRAssignment2/src/main/java/org/example/Main.java
@@ -4,7 +4,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> docs = new WebCrawler().crawl(10);
-        System.out.println(docs);
+        // Initialize WebCrawler and InvertedIndex
+        WebCrawler crawler = new WebCrawler();
+        Map<Integer, String> docs = crawler.crawl(10);
+
+        // Build Inverted Index
+        InvertedIndex index = new InvertedIndex();
+        index.buildIndex(docs);
+
+//       uncomment law 3ayz t4of el inverted index 
+//        index.printIndex();
+
     }
 }
